f4b308598aa557cbbffcbb9da14ac93656277aa6
Dzmitry <87767424+dzmitrySkr@users.noreply.github.com>
2022-02-05 12:10:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
- let obj = {};
+let obj = {};
   let stack = [];
   for(let i = 0; i < bracketsConfig.length; i++){
     obj[bracketsConfig[i][0]] = bracketsConfig[i][1];
   }
-  //console.log(obj)
   for(let i = 0; i <str.length; i++){
     let current = str[i];
-    if (current === '|'  current === '8'  current === '7') {
+    if (current === '|' || current === '8' || current === '7') {
       let top = stack[stack.length-1];
       if (top !== current){
         stack.push(current);
-        console.log(current,top)
+
       } else {
         stack.pop(top);
       }
@@ -35,4 +34,5 @@ module.exports = function check(str, bracketsConfig) {
 
   }
    return (stack.length === 0)
+
 }
